00d30a79e6c4217acb6f1f44bf847e0f39c5c8e8
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-22 18:06:17 -0500

    Add Devise + Quick Updates

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      mainBlue: '#3778c2',
+      secondGreen: '#5fb523',
+      textGray: '#434b54',
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
