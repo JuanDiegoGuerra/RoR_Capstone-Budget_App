4904c3aa011886647c8bc650f198d3ed3b36dc06
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-22 19:19:11 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+    resources :groups, only: [:index, :new, :create, :show, :destroy], path: 'categories' do
+    resources :purchases, only: [:new, :create ], path: 'transactions'
+  end
   resources :groups, only: [:index, :new, :create, :show]
   root 'groups#index'
 end
