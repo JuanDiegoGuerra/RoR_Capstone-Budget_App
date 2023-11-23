1fd583d886e77ce8418841d69733e4e444b9b22e
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-23 13:13:45 -0500

    Rubocop Quick Fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,9 +38,9 @@ RSpec.configure do |config|
   end
 
   config.before(:suite) do
-    if config.files_to_run.any? { |path| path.start_with?(Rails.root.join("spec/system").to_s) }
+    if config.files_to_run.any? { |path| path.start_with?(Rails.root.join('spec/system').to_s) }
       Rails.application.load_tasks
-      Rake::Task["tailwindcss:build"].invoke
+      Rake::Task['tailwindcss:build'].invoke
     end
   end
 
