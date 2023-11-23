8c4ff0e7c538065df0734bc26e5d72e5c776128b
Juan Diego Guerra <jdxelric@gmail.com>
2023-11-22 19:18:56 -0500

    Update: Controllers + Helpers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,8 +6,21 @@ class GroupsController < ApplicationController
     @groups = @user.groups.includes(:purchases)
   end
 
+  def show
+    @group = current_user.groups.includes(:purchases).order('purchases.created_at DESC').find(params[:id])
+  end
+
   def new
     @group = Group.new
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+
+    respond_to do |format|
+      format.html { redirect_to groups_path, notice: 'Category was successfully destroyed.' }
+    end
   end
 
   def create
@@ -17,7 +30,7 @@ class GroupsController < ApplicationController
       if @group.save
         format.html { redirect_to groups_path, notice: 'Category was successfully created.' }
       else
-        flash.now[:alert] = 'The arguments are invalid.'
+        flash.now[:alert] = @group.errors.full_messages.join(', ')
         format.html { render :new, status: :unprocessable_entity }
       end
     end
